e0839c6aed3f3683d9f91e23751918ca070a3839
Murad Ahmed <murad@linkstaff.co.jp>
2022-09-27 20:05:19 +0600

    database connection created and data get succesfull

--- database/migrations/2022_09_27_133928_create_employees_table.php
+++ database/migrations/2022_09_27_133928_create_employees_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('team');
             $table->timestamps();
         });
     }
