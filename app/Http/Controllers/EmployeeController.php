b1f9090356d67c36df94088ced8e538c45207526
Murad Ahmed <murad@linkstaff.co.jp>
2022-09-27 20:11:30 +0600

    Employee post function added in controller

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,83 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return Employee::all();
+       return Employee::orderBy('created_at', 'desc')->get();
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $newItem = new Employee;
+        $newItem->name = $request->item['name'];
+        $newItem->save();
+ 
+        return $newItem;
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $existingItem = Employee::find($id);  
+ 
+        if($existingItem){
+           $existingItem->completed = $request->item['completed'] ? true : false;
+           $existingItem->updated_at = Carbon::now() ;
+           $existingItem->save();
+           return $existingItem;
+ 
+        } 
+        return "Item not found";
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $existingItem = Employee::find($id);
+        if($existingItem){
+           $existingItem->delete();
+           return "Item deleted";
+        }
+        return "Item not found";
     }
 }
